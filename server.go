eb5365a9e7abea808e7825fe263e773a0048b35f
Wes Freeman <wes@skeweredrook.com>
2013-10-16 03:30:14 -0400

    adding TLS

--- server.go
+++ server.go
@@ -5,13 +5,13 @@ import (
 )
 
 func baseHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  w.Write([]byte("{\"message\":\"hello world\"}"))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("{\"message\":\"hello world\"}"))
 }
 
 func main() {
 	servemux := http.NewServeMux()
-   servemux.HandleFunc("/", baseHandler)
+	servemux.HandleFunc("/", baseHandler)
 
-   http.ListenAndServe("localhost:4321", servemux)
+	http.ListenAndServeTLS("localhost:4321", "cert.pem", "key.pem", servemux)
 }
--- server_test.go
+++ server_test.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"crypto/tls"
+	"io/ioutil"
 	"net/http"
 	"testing"
-   "time"
-   "io/ioutil"
+	"time"
 )
 
 func TestBaseHandler(t *testing.T) {
 	go main()
-	time.Sleep(100 * time.Millisecond)
 
-	resp, _ := http.Get("http://localhost:4321/")
+	time.Sleep(200 * time.Millisecond)
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, _ := client.Get("https://localhost:4321/")
 	body, _ := ioutil.ReadAll(resp.Body)
 	if string(body) != "{\"message\":\"hello world\"}" {
 		t.Error("TestBaseHandler doesn't match:", body)
